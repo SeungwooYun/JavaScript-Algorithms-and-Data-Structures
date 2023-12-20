36eb4c3d059f004a1a46ab2b6954ad9b1c9cae53
SeungwooYun <103883076+SeungwooYun@users.noreply.github.com>
2023-12-20 01:56:04 +0000

    binarysearch done

--- 8_binarySearch.js
+++ 8_binarySearch.js
@@ -3,9 +3,8 @@
 // Array sort 먼저 진행할 것 
 
 
-let arr = [1, 2, 3, -6, -10, 6, 7, 8, 9, 10]
+let arr = [1, 2, 3, -6, -10, 6, 7, 8, 9]
 function binarySearch (arr, t) {
-    // sorting 어떻게 하지? 0부터 마지막까지 한바퀴 돌면서 두개 비교했을 때 무조건 뒷 인덱스가 더 크게 
     for (let j = 0; j < arr.length ; j++){
         for (let i = 0; i< arr.length; i++){
         if (arr[i] > arr[i+1]){
@@ -19,49 +18,59 @@ function binarySearch (arr, t) {
     // 솔팅완료 
     console.log("sorted array", arr)
     
-    // n은 인자로 받은 array 의 중앙값. 소수의 경우 반올림
-    let n = Math.round(arr.length / 2);
-    console.log("첫 평균인덱스", n)
-    console.log("첫 평균밸류", arr[n])
+//     // n은 인자로 받은 array 의 중앙값. 소수의 경우 반올림
+    let n = Math.round(arr.length / 2)-1;
+    console.log("첫 n", n)
+    console.log("첫 arr[n]", arr[n])
     
     let min = 0;
     let max = arr.length-1;
     if(arr[n]===t){return n};
+
+    // 배열 바깥의 수가 들어온 경우 
+    if(arr[0] > t || arr[arr.length-1] < t){
+        return -1
+     }
+
+         // 첫 n번째 인덱스가 t가 아닌경우 while loop.
     while (arr[n]!==t){
-        if(arr[0] > t){
-            break
-        } else if (arr[arr.length-1] < t){
-            break
-        } else if (arr[n] < t) {
-            // console.log("arr[n]", arr[n])
-            // console.log("t", t)
-            // n번째 인덱스가 t보다 작으면, 찾아야하는 t는더 배열의 오른쪽에 있다는 것. 
+        // 솔팅된 배열의 첫번쨰(가장 작은 수) 보다 t(찾는 수)가 큰 경우 
+       if (arr[n] < t) {
+            // t는더 배열의 오른쪽에 있다는 것. 
             // - min 은 n 다음밸류부터. 
             min = n+1;
             // n은 기존 max 와 기존n의 평균값 
             n = Math.round((min + max)/2)
-            console.log("test1", n)
+            if( t < arr[min]){
+                return -1 
+            }
         } else if (arr[n] > t){
-            console.log(arr)
-            console.log("n", n)
-            console.log("arr[n]", arr[n])
-            console.log("t", t)
         // n번째 밸류의 값이 t보다 큰 경우 t는 배열의 왼 쪽에 있음. 
         // 최대로 확인해야할 인덱스는 n보다 하나 적을듯.
             max = n-1
             // min부터 n 사이 중간값을 다시 확인해줘야함
             n = Math.round((min+max)/2)
-            // console.log("test2", n)
+            if( t > arr[max]){
+                return -1 
+            }
         }
     }
-    if(arr[n]===t){return n} else {
-    return -1
-}
+    if(arr[n]===t){return n} 
 }
 
 // 만약에 더 큰 경우에는 뒤에있는 것들의 중간값을 봐줘야함 
 
-console.log(binarySearch(arr, -5))
+console.log(binarySearch(arr, 5))
+
+
+
+
+
+
+
+
+
+
     // for (let i=0 ; i < arr.length; i++){
     //     if(arr[i] === t) {
     //         console.log(arr)
